ffe9ada990013f614ae055def786ccd8020ab982
Fátima Alfaro <fatialfaro99@gmail.com>
2026-04-22 20:27:07 -0300

    Agregue los estilos del navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <p >
           Tu catálogo de cine personal.
         </p>
-        <p >&copy; {new Date().getFullYear()} | Desarrollado por <a href="#" >Emi</a></p>
+        <p >&copy; {new Date().getFullYear()} | Desarrollado por <a href="#" >Fátima Alfaro</a></p>
         <p >Realizado con React & Tailwind CSS v4</p>
       </footer>
     </>
