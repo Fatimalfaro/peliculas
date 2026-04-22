0b1cd2f9fa55ade7effb8999ecfb245661593044
Fátima Alfaro <fatialfaro99@gmail.com>
2026-04-22 19:41:12 -0300

    Agregue los componentes del proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p >
+          Tu catálogo de cine personal.
+        </p>
+        <p >&copy; {new Date().getFullYear()} | Desarrollado por <a href="#" >Emi</a></p>
+        <p >Realizado con React & Tailwind CSS v4</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
